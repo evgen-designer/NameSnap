36a93c0ca7abaae6043be850f3d18d77f8da694f
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-11-07 07:55:59 +0000

    Initial Commit

---
+++ NameSnap/NameSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameSnapApp.swift
+//  NameSnap
+//
+//  Created by Mac on 07/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NameSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
